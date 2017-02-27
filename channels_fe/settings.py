d2e2f541db12b8b6032cc719fb9e524f9f210f06
Emma <emma.upton.95@gmail.com>
2017-02-27 20:27:07 +0000

    fix

--- channels_fe/settings.py
+++ channels_fe/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'channels_fe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
